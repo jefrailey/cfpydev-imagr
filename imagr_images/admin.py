45d9e721fc12d1ef808bf020968c9ab0a15f59aa
corinnelhh <corinne.hutchinson@gmail.com>
2014-07-07 20:32:28 -0700

    PEP 8

--- imagr_images/admin.py
+++ imagr_images/admin.py
@@ -5,13 +5,20 @@ from imagr_images.models import Photo, Album
 class AlbumAdmin(admin.ModelAdmin):
     readonly_fields = ('date_created', 'date_modified')
     list_display = ('owner', 'title')
-    search_fields = ('owner__username','owner__first_name', 'owner__last_name', 'owner__email')
+    search_fields = (
+        'owner__username', 'owner__first_name',
+        'owner__last_name', 'owner__email'
+    )
 
 
 class PhotoAdmin(admin.ModelAdmin):
     readonly_fields = ('date_uploaded', 'date_modified')
     list_display = ('owner', 'title', 'image_size', 'date_uploaded')
     list_filter = ('date_uploaded',)
-    search_fields = ('owner__username','owner__first_name', 'owner__last_name', 'owner__email')
+    search_fields = (
+        'owner__username', 'owner__first_name',
+        'owner__last_name', 'owner__email'
+    )
+
 admin.site.register(Album, AlbumAdmin)
 admin.site.register(Photo, PhotoAdmin)
