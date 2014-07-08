d5cf0ae3bb0cbaa2366a7e5530e4ad7a3edd5bf7
jefrailey <jcaetof@gmail.com>
2014-07-08 16:21:16 -0700

    Add _get_owner_album() to get a user's albums; add home_page() view

--- imagr_images/views.py
+++ imagr_images/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse as Response
+from imagr_images.models import Photo
+from imagr_images.models import Album
 
 
 # Create your views here.
-def front_page():
+def front_page(request):
     u"""shows anonymous users something nice to encourage them to sign up"""
-    pass
+    return Response('You want to share with us. Sign up now!')
 
 
-def home_page():
+def home_page(request, owner):
     u"""shows logged-in users a list of their albums, with a representative
     image from each album
     """
-    pass
+    if request.user.is_authenticated():
+        return Response(_get_owner_album(request.user.id))
+
+
+def _get_owner_album(owner_id):
+    return Album.objects.filter(owner=owner_id)
 
 
 def album_page():
