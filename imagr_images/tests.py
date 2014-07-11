c2587851837b5a8b5c17653e3610b38c6ba06e37
jefrailey <jcaetof@gmail.com>
2014-07-10 19:15:40 -0700

    Add skeleton tests for imager_images models

--- imagr_images/tests.py
+++ imagr_images/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
 
-# Create your tests here.
+
+class ImagesTests(TestCase):
+    def setUp(self):
+        pass
+
+    def test_published_between(self):
+        u"""Photo test"""
+        pass
+
+    def test_save(self):
+        u"""Photo test"""
+        pass
+
+    def test_owner_link(self):
+        u"""Album test"""
+        pass
+
+    def test_cover(self):
+        u"""Album test"""
+        pass
+
+    def test_all_photos(self):
+        u"""Album test"""
+        pass
