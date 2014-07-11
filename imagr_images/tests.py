6052b845aaf73a7c29f5c95ed691716c830c6dba
jefrailey <jcaetof@gmail.com>
2014-07-10 20:02:44 -0700

    Add test for phot save

--- imagr_images/tests.py
+++ imagr_images/tests.py
@@ -1,18 +1,60 @@
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.conf import settings
+from imagr_images.models import Photo, Album
+from imagr_users.models import ImagrUser
+from django.core.files import File
+import os
+import datetime
 
 
 class ImagesTests(TestCase):
     def setUp(self):
-        pass
+        u"""Create a user and image that can be used in all tests.
 
-    def test_published_between(self):
-        u"""Photo test"""
-        pass
+        Create a temp folder for images to be stored in, so test images
+        don't clog up regular media folder."""
+
+        TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+        self._old_MEDIA_ROOT = settings.MEDIA_ROOT
+
+        # override MEDIA_ROOT for this test
+        settings.MEDIA_ROOT = os.path.join(TEST_ROOT, 'test_data/media/')
+
+        user = ImagrUser(username="tester")
+        user.save()
+
+        with open("test_images/1.png", 'rb') as file_:
+            a_photo = File(file_)
+            photo = Photo(title=u"tester's Photo", image=a_photo,
+                          privacy=0, owner=user)
+            photo.save()
+
+        with open("test_images/2.png", 'rb') as file_:
+            a_photo = File(file_)
+            photo = Photo(title=u"tester's Second Photo", image=a_photo,
+                          privacy=0, owner=user)
+            photo.save()
+
+        album = Album(title="tester's Album", privacy=0, owner=user)
+        album.save()
+        album2 = Album(title="tester's Second Album", privacy=0, owner=user)
+        album2.save()
+
+    def tearDown(self):
+        # reset MEDIA_ROOT
+        settings.MEDIA_ROOT = self._old_MEDIA_ROOT
+
+    # def test_published_between_true(self):
+    #     u"""Photo test"""
+    #     photo = Photo.objects.all()[0]
+    #     self.assertEqual(photo.published_between(, False)
 
     def test_save(self):
         u"""Photo test"""
-        pass
+        photo = Photo.objects.all()[0]
+        self.assertGreater(photo, 0)
 
     def test_owner_link(self):
         u"""Album test"""
